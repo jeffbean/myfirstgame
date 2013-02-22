dd37a3643c8b5e3eed197d695ae5f0fd99b3faaf
Jeffrey Bean <jeffreyrobertbean@gmail.com>
2013-02-21 22:36:19 -0500

    finish tutorial - colision and menu

--- src/javagame/Game.java
+++ src/javagame/Game.java
@@ -1,36 +1,39 @@
 package javagame;
-import org.newdawn.slick.*;
-import org.newdawn.slick.state.*;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
 public class Game extends StateBasedGame {
 
 	public static final String gamename = "Don't know yet";
 	public static final int menu = 0;
 	public static final int play = 1;
-	
-	public Game(String gamename){
+
+	public Game(String gamename) {
 		super(gamename);
 		this.addState(new Menu(menu));
 		this.addState(new Play(play));
 	}
-	
+
 	@Override
 	public void initStatesList(GameContainer gc) throws SlickException {
-		// Need to add the states 
+		// Need to add the states
 		this.getState(menu).init(gc, this);
 		this.getState(play).init(gc, this);
 		// What state to go to first
 		this.enterState(menu);
 	}
-	
-	public static void main (String[] args) {
+
+	public static void main(String[] args) {
 		AppGameContainer appgc;
 		try {
 			appgc = new AppGameContainer(new Game(gamename));
 			appgc.setDisplayMode(640, 360, false);
 			appgc.start();
-		}catch(SlickException e){
+		} catch (SlickException e) {
 			e.printStackTrace();
 		}
-	}	
-} 
+	}
+}
--- src/javagame/Play.java
+++ src/javagame/Play.java
@@ -4,6 +4,7 @@ import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -28,11 +29,15 @@ public class Play extends BasicGameState {
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
 		worldMap = new Image("res/world.png");
-		Image[] walkUp = {new Image("res/buckysBack.png"), new Image("res/buckysBack.png")};
-		Image[] walkDown = {new Image("res/buckysFront.png"), new Image("res/buckysFront.png")};
-		Image[] walkLeft = {new Image("res/buckysLeft.png"), new Image("res/buckysLeft.png")};
-		Image[] walkRight = {new Image("res/buckysRight.png"), new Image("res/buckysRight.png")};
-		
+		Image[] walkUp = { new Image("res/buckysBack.png"),
+				new Image("res/buckysBack.png") };
+		Image[] walkDown = { new Image("res/buckysFront.png"),
+				new Image("res/buckysFront.png") };
+		Image[] walkLeft = { new Image("res/buckysLeft.png"),
+				new Image("res/buckysLeft.png") };
+		Image[] walkRight = { new Image("res/buckysRight.png"),
+				new Image("res/buckysRight.png") };
+
 		movingUp = new Animation(walkUp, duration, false);
 		movingDown = new Animation(walkDown, duration, false);
 		movingLeft = new Animation(walkLeft, duration, false);
@@ -42,16 +47,17 @@ public class Play extends BasicGameState {
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		
+
 		worldMap.draw(playerPositionX, playerPositionY);
 		player.draw(shiftX, shiftY);
-		g.drawString("Player X:" + playerPositionX + " Y:" + playerPositionY, 100, 10);
-		
-		if(quit==true){
+		g.drawString("Player\nX:" + playerPositionX + "\nY:" + playerPositionY,
+				100, 10);
+
+		if (quit == true) {
 			g.drawString("Resume (R)", 250, 100);
 			g.drawString("Main Menu (M)", 250, 120);
 			g.drawString("Quit Game (Q)", 250, 140);
-			if(quit==false){
+			if (quit == false) {
 				g.clear();
 			}
 		}
@@ -59,7 +65,56 @@ public class Play extends BasicGameState {
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
-
+		Input input = gc.getInput();
+		// move up
+		if (input.isKeyDown(Input.KEY_UP)) {
+			player = movingUp;
+			playerPositionY += delta * .1f;
+			if (playerPositionY > 172) {
+				playerPositionY -= delta * .1f;
+			}
+		}
+		// move down
+		if (input.isKeyDown(Input.KEY_DOWN)) {
+			player = movingDown;
+			playerPositionY -= delta * .1f;
+			if (playerPositionY < -600) {
+				playerPositionY += delta * .1f;
+			}
+		}
+		// move left
+		if (input.isKeyDown(Input.KEY_LEFT)) {
+			player = movingLeft;
+			playerPositionX += delta * .1f;
+			if (playerPositionX > 324) {
+				playerPositionX -= delta * .1f;
+			}
+		}
+		// move right
+		if (input.isKeyDown(Input.KEY_RIGHT)) {
+			player = movingRight;
+			playerPositionX -= delta * .1f;
+			if (playerPositionX < -840) {
+				playerPositionX += delta * .1f;
+			}
+		}
+		
+		// escape menu 
+		if(input.isKeyDown(Input.KEY_ESCAPE)){
+			quit = true;
+		}
+		//when the menu is on the screen
+		if(quit==true){
+			if(input.isKeyDown(Input.KEY_R)){
+				quit = false;
+			}
+			if(input.isKeyDown(Input.KEY_M)){
+				sbg.enterState(0);
+			}
+			if(input.isKeyDown(Input.KEY_Q)){
+				System.exit(0);
+			}
+		}
 	}
 
 	public int getID() {
