c5a6f40336b83ad2a5ecf05bad6776743d0098cd
Jeffrey Bean <jeffreyrobertbean@gmail.com>
2013-02-21 20:35:14 -0500

    add natives

--- src/javagame/Game.java
+++ src/javagame/Game.java
@@ -1,45 +1,26 @@
 package javagame;
-import org.newdawn.slick.Animation;
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.SpriteSheet;
-import org.newdawn.slick.tiled.TiledMap;
- 
-public class Game extends BasicGame {
-	private float playerX=320;
-	private float playerY=240;
-	private TiledMap map;	
-	private Animation player;
-	public Game() {
-		super("one class barebone game");
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Game extends StateBasedGame {
+
+	public static final String gamename = "Don't know yet";
+	public static final int menu = 0;
+	public static final int play = 1;
+	
+	public Game(String gamename){
+		super(gamename);
+		this.addState(new Menu(menu));
+		this.addState(new Play(play));
 	}
-	public void init(GameContainer container) throws SlickException {
-		container.setVSync(true);
-		SpriteSheet sheet = new SpriteSheet("data/karbonator.png", 32 ,32 );
-		map = new TiledMap("data/map02.tmx");
-		player = new Animation();
-		player.setAutoUpdate(true);
-		for (int frame=0;frame<3;frame++) {
-			player.addFrame(sheet.getSprite(frame,0), 150);
-		}		
+	
+	public static void main (String[] args) {
+		
 	}
-	public void update(GameContainer container, int delta) { 
-		if (container.getInput().isKeyDown(Input.KEY_LEFT)) {playerX--;}
-		if (container.getInput().isKeyDown(Input.KEY_RIGHT)) {playerX++;}
-		if (container.getInput().isKeyDown(Input.KEY_UP)) {playerY--;}
-		if (container.getInput().isKeyDown(Input.KEY_DOWN)) {playerY++;}
-	} 
-	public void render(GameContainer container, Graphics g)  {
-		map.render(0, 0);
-		g.drawAnimation(player, playerX, playerY);
+
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		
 	}
-	public static void main(String[] argv) throws SlickException {
-		AppGameContainer container = 
-			new AppGameContainer(new Game(), 640, 480, false);
-		container.start();
-	}
-}
+} 
