62c17eb1866d6b2b00bb3bbeebd0fc26d8f49a2d
Jeffrey Bean <jeffreyrobertbean@gmail.com>
2013-02-21 21:13:01 -0500

    added image and mouse input traking

--- src/javagame/Menu.java
+++ src/javagame/Menu.java
@@ -1,12 +1,16 @@
 package javagame;
 
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Menu extends BasicGameState {
+	
+	public String mouse = "No input yet!";
 	
 	public Menu(int state){
 		
@@ -17,13 +21,21 @@ public class Menu extends BasicGameState {
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-		g.drawString("Lets play this game!", 50, 50);
+		g.drawString(mouse, 50, 50);
 		g.drawRect(50, 100, 60, 120); //x, y, width, height
-		g.drawOval(200, 130, 130, 80); //x, y, width, height
+		
+		// g.drawOval(200, 130, 130, 80); //x, y, width, height
+		Image face = new Image("res/jbean.png");
+		g.drawImage(face, 200, 130);
+		
+		
 	}
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
+		int xpos = Mouse.getX();
+		int ypos = Mouse.getY();
 		
+		mouse = "Mouse position x:" + xpos + " y: " + ypos;
 	}
 	
 	public int getID(){
