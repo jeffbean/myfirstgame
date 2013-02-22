17f939bb0ed2d4454fff6a77bb1905579fa88bc4
Jeffrey Bean <jeffreyrobertbean@gmail.com>
2013-02-21 21:23:57 -0500

    now my face is moving based on arrow keys

--- src/javagame/Menu.java
+++ src/javagame/Menu.java
@@ -4,38 +4,56 @@ import org.lwjgl.input.Mouse;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Menu extends BasicGameState {
 	
-	public String mouse = "No input yet!";
+	public String mouseString = "No input yet!";
+	Image face;
+	int faceX = 200;
+	int faceY = 200;
 	
 	public Menu(int state){
 		
 	}
 
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
+		face = new Image("res/jbean.png");
 		
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-		g.drawString(mouse, 50, 50);
-		g.drawRect(50, 100, 60, 120); //x, y, width, height
+		g.drawString(mouseString, 100, 10);
+		//g.drawRect(50, 100, 60, 120); //x, y, width, height
 		
 		// g.drawOval(200, 130, 130, 80); //x, y, width, height
-		Image face = new Image("res/jbean.png");
-		g.drawImage(face, 200, 130);
+		//Image face = new Image("res/jbean.png");
+		//g.drawImage(face, 200, 130);
 		
-		
+		g.drawImage(face, faceX, faceY);
 	}
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
 		int xpos = Mouse.getX();
 		int ypos = Mouse.getY();
+		mouseString = "Mouse x:" + xpos + " y: " + ypos;
 		
-		mouse = "Mouse position x:" + xpos + " y: " + ypos;
+		Input input = gc.getInput();
+		if(input.isKeyDown(Input.KEY_UP)){
+			faceY -= 1;
+		}
+		if(input.isKeyDown(Input.KEY_DOWN)){
+			faceY += 1;
+		}
+		if(input.isKeyDown(Input.KEY_LEFT)){
+			faceX -= 1;
+		}
+		if(input.isKeyDown(Input.KEY_RIGHT)){
+			faceX += 1;
+		}
 	}
 	
 	public int getID(){
