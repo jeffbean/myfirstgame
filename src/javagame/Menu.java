5adbbb909e654bd3a2a4d6ec85e07be30664b3b8
Jeffrey Bean <jeffreyrobertbean@gmail.com>
2013-02-21 21:50:04 -0500

    Now have play game and exit buttons
    Deleted comments due to source control

--- src/javagame/Menu.java
+++ src/javagame/Menu.java
@@ -12,7 +12,11 @@ import org.newdawn.slick.state.StateBasedGame;
 public class Menu extends BasicGameState {
 
 	public String mouseString = "No input yet!";
-	Image face;
+	public String welcome = "Welcome to JBean land!";
+	
+	Image playNow;
+	Image exitGame;
+	
 	int faceX = 200;
 	int faceY = 200;
 
@@ -22,25 +26,18 @@ public class Menu extends BasicGameState {
 
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
-		face = new Image("res/jbean.png");
 
+		playNow = new Image("res/playNow.png");
+		exitGame = new Image("res/exitGame.png");
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		g.drawString(mouseString, 100, 10);
-		// g.drawRect(50, 100, 60, 120); //x, y, width, height
-
-		// g.drawOval(200, 130, 130, 80); //x, y, width, height
-
-		// This is a simple image draw on the screen
-		// Image face = new Image("res/jbean.png");
-		// g.drawImage(face, 200, 130);
-
-		// g.drawImage(face, faceX, faceY);
-
-		g.fillOval(75, 100, 100, 100);
-		g.drawString("Play Now!", 80, 70);
+		g.drawString(mouseString, 101, 10);
+		g.drawString(welcome, 100, 50);
+		playNow.draw(100, 100);
+		exitGame.draw(100, 200);
+	
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
@@ -49,26 +46,20 @@ public class Menu extends BasicGameState {
 		int xpos = Mouse.getX();
 		int ypos = Mouse.getY();
 		mouseString = "Mouse x:" + xpos + " y: " + ypos;
-
-		if (input.isKeyDown(Input.KEY_UP)) {
-			faceY -= 1;
-		}
-		if (input.isKeyDown(Input.KEY_DOWN)) {
-			faceY += 1;
-		}
-		if (input.isKeyDown(Input.KEY_LEFT)) {
-			faceX -= 1;
-		}
-		if (input.isKeyDown(Input.KEY_RIGHT)) {
-			faceX += 1;
-		}
-		
-		if((xpos > 75 && xpos < 175) && ( ypos > 160 && ypos < 260)){
-			//this will run inside the circle
+		//Play Now Button
+		if((xpos > 100 && xpos < 311) && (ypos > 209 && ypos < 260)) {
 			if(input.isMouseButtonDown(0)){
 				sbg.enterState(1);
 			}
 		}
+		//Exit Game
+		if((xpos > 100 && xpos < 311) && (ypos > 109 && ypos < 160)) {
+			if(input.isMouseButtonDown(0)){
+				System.exit(0);
+			}
+		}
+		
+
 	}
 
 	public int getID() {
