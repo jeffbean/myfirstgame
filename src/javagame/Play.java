057ab361150a61bccd68b1e5e1135d27a0e2ea2e
Jeffrey Bean <jeffreyrobertbean@gmail.com>
2013-02-21 22:07:09 -0500

    added vars and init for Play state

--- src/javagame/Play.java
+++ src/javagame/Play.java
@@ -1,30 +1,56 @@
 package javagame;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Play extends BasicGameState {
-	
-	public Play(int state){
-		
+
+	Animation player, movingUp, movingDown, movingLeft, movingRight;
+	Image worldMap;
+	boolean quit = false;
+	int[] duration = { 200, 200 };
+	float playerPositionX = 0;
+	float playerPositionY = 0;
+
+	// this is for centering player in the middle of the screen
+	float shiftX = playerPositionX + 320;
+	float shiftY = playerPositionY + 160;
+
+	public Play(int state) {
+
 	}
 
-	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		worldMap = new Image("res/world.png");
+		Image[] walkUp = {new Image("res/buckysBack.png"), new Image("res/buckysBack.png")};
+		Image[] walkDown = {new Image("res/buckysFront.png"), new Image("res/buckysFront.png")};
+		Image[] walkLeft = {new Image("res/buckysLeft.png"), new Image("res/buckysLeft.png")};
+		Image[] walkRight = {new Image("res/buckysRight.png"), new Image("res/buckysRight.png")};
 		
+		movingUp = new Animation(walkUp, duration, false);
+		movingDown = new Animation(walkDown, duration, false);
+		movingLeft = new Animation(walkLeft, duration, false);
+		movingRight = new Animation(walkRight, duration, false);
+		player = movingDown;
 	}
-	
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
 		g.drawString("This is the play state!", 100, 100);
 	}
-	
-	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
-		
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
+
 	}
-	
-	public int getID(){
+
+	public int getID() {
 		return 1;
 	}
 
